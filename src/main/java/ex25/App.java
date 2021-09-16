bae774c882abcb3a758ff8e21614c87b2a65dc2a
Ryan <rdturner40@gmail.com>
2021-09-16 14:34:08 -0400

    first commit

---
+++ src/main/java/ex25/App.java
@@ -0,0 +1,8 @@
+package ex25;
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/ex28/App.java
@@ -0,0 +1,9 @@
+package ex28;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
