e4a95c4d098ffb4aced9a09bd4f188955b33b0d7
Ryan <rdturner40@gmail.com>
2021-09-21 11:20:27 -0400

    MVP Ex 31

--- src/main/java/ex31/App.java
+++ src/main/java/ex31/App.java
@@ -5,10 +5,51 @@ package ex31;
  *  Copyright 2021 Ryan Turner
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App
 {
-    public static void main( String[] args )
+    public static void calculateKarvonen(int age, int heartRate) {
+        System.out.format("");
+        System.out.println("Intensity   |  Rate");
+        System.out.println();
+        System.out.format("--------------------");
+        System.out.println();
+        for (double i = 0.55; i < 0.96; i+=0.05) {
+            int intensity = (int) (i * 100);
+            int target = (int) ((((220 - age) - heartRate) * i) + heartRate);
+            System.out.format("%-10s %-10s",intensity, "|  " + target);
+            System.out.println();
+        }
+    }
+
+    public static void getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int heartRate = 0;
+        int age = 0;
+        boolean flag = true;
+
+        while(flag) {
+            try {
+                if (age == 0) {
+                    System.out.println("Please enter your age: ");
+                    age = Integer.parseInt(reader.readLine());
+                }
+                if (heartRate == 0) {
+                    System.out.println("Please enter your resting heart rate: ");
+                    heartRate = Integer.parseInt(reader.readLine());
+                }
+                flag = false;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter a valid number");
+            }
+        }
+        calculateKarvonen(age, heartRate);
+    }
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        getInput();
     }
 }
