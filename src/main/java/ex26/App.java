65cea6496d5980f8a15deab742cacfbd34563ee0
Ryan <rdturner40@gmail.com>
2021-09-21 09:45:41 -0400

    MVP Ex 27

--- src/main/java/ex26/App.java
+++ src/main/java/ex26/App.java
@@ -5,10 +5,79 @@ package ex26;
  *  Copyright 2021 Ryan Turner
  */
 
-public class App
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class PaymentCalculator
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void calculateMonthsUntilPaidOff(double trueApy, double balance, double paymentAmount) {
+    // n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+        System.out.println(trueApy);
+        System.out.println(balance);
+        System.out.println(paymentAmount);
+        double months = -(1.0 / 30) * Math.log(1 + (balance / paymentAmount) * (1 - Math.pow((1 + trueApy), 30)) / Math.log(1 + trueApy));
+        System.out.println(months);
     }
 }
+
+public class App
+{
+    public static double convertAPR(double apr) {
+        double apiVal = apr / 100;
+        System.out.println(apiVal);
+        return apiVal / 360;
+    }
+
+    public static void getInput() throws IOException {
+        double balance;
+        double percentage;
+        double paymentAmount;
+        double trueApy;
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("What is your balance? ");
+        balance = Double.parseDouble(String.valueOf(reader.readLine()));
+
+        System.out.println("What is the APR on the card (as a percent)? ");
+        percentage = Double.parseDouble(String.valueOf(reader.readLine()));
+
+        System.out.println("What is the monthly payment you can make? ");
+        paymentAmount = Double.parseDouble(String.valueOf(reader.readLine()));
+        // Convert input percentage to a decimal and convert to apy.
+        trueApy = convertAPR(percentage);
+        PaymentCalculator.calculateMonthsUntilPaidOff(trueApy, balance, paymentAmount);
+    }
+
+    public static void main( String[] args ) throws IOException {
+        getInput();
+    }
+}
+/*
+* It can take a lot longer to pay off your credit card balance than you might realize.
+* And the formula for figuring that out isn’t pretty. Hiding the formula’s complexity with a function can help you keep your code organized.
+
+Write a program that will help you determine how many months it will take to pay off
+* a credit card balance. The program should ask the user to enter the balance of a credit card,
+* the APR of the card, and their monthly payment. The program should then return the number of months needed (rounded up to the next integer value).
+
+The formula for this is
+
+n = -(1/30) * log(1 + b/p * (1 - (1 + i)^30)) / log(1 + i)
+where
+
+n is the number of months.
+i is the daily rate (APR divided by 365).
+b is the balance.
+p is the monthly payment.
+Example Output
+What is your balance? 5000
+What is the APR on the card (as a percent)? 12
+What is the monthly payment you can make? 100
+It will take you 70 months to pay off this card.
+Constraints
+Prompt for the card’s APR. Do the division internally.
+Prompt for the APR as a percentage, not a decimal.
+Use a class called PaymentCalculator with a public method called calculateMonthsUntilPaidOff, which returns the number of months.
+Round fractions of a cent up to the next cent.
+* */
--- src/main/java/ex27/App.java
+++ src/main/java/ex27/App.java
@@ -5,10 +5,106 @@ package ex27;
  *  Copyright 2021 Ryan Turner
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Pattern;
+
+class HandleVerification
+{
+    public static boolean fNamePasses;
+    private static boolean lNamePasses;
+    private static boolean zipCodePasses;
+    private static boolean eIDPasses;
+
+    public static void checkEID(String id) {
+        String regex = "^[a-zA-Z]{2}-[0-9]{4}$";
+        if (Pattern.matches(regex, id)) {
+            eIDPasses = true;
+        }
+    }
+
+    public static void checkZipCode(String zip) {
+        if(Pattern.matches("^[0-9]{5}$", zip)) {
+            zipCodePasses = true;
+        }
+    }
+
+    public static void checkLName(String lName) {
+        if (lName != null && !lName.trim().isEmpty() && lName.length() >= 2) {
+            lNamePasses = true;
+        }
+    }
+
+    public static void checkFName(String fName) {
+        if (fName != null && !fName.trim().isEmpty() && fName.length() >=2) {
+            fNamePasses = true;
+        }
+    }
+
+    public static boolean getEIDPasses () {
+        return eIDPasses;
+    }
+
+    public static boolean getZipCodePasses() {
+        return zipCodePasses;
+    }
+
+    public static boolean getLNamePasses() {
+        return lNamePasses;
+    }
+
+    public static boolean getFNamePasses() {
+        return fNamePasses;
+    }
+}
+
 public class App
 {
-    public static void main( String[] args )
+    public static void generateMessage(String fName, String lName, String zip, String eId) {
+        if (HandleVerification.getFNamePasses()
+                && HandleVerification.getLNamePasses()
+                && HandleVerification.getEIDPasses()
+                && HandleVerification.getZipCodePasses()) {
+            System.out.println("All of your entries are valid.");
+        } else if (!HandleVerification.getFNamePasses()) {
+            System.out.println("The first name " + fName +" is not valid. The entry must contain two characters.");
+        } else if (!HandleVerification.getLNamePasses()) {
+            System.out.println("The last name " +lName +" is not valid. The entry must contain two characters ");
+        } else if (!HandleVerification.getZipCodePasses()) {
+            System.out.println("The zip code " + zip +" is not valid. It must be a 5 digit number.");
+        } else if (!HandleVerification.getEIDPasses()){
+            System.out.println("The EID " + eId + " is not valid. Must follow format `AA-1234`");
+        }
+    }
+
+    public static void getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fName;
+        String lName;
+        String zipCode;
+        String eID;
+        System.out.println("Enter the first name");
+        fName = reader.readLine();
+        HandleVerification.checkFName(fName);
+
+        System.out.println("Enter the last name");
+        lName = reader.readLine();
+        HandleVerification.checkLName(lName);
+
+        System.out.println("Enter the ZIP code");
+        zipCode = reader.readLine();
+        HandleVerification.checkZipCode(zipCode);
+
+        System.out.println("Enter the employee ID: ");
+        eID = reader.readLine();
+        HandleVerification.checkEID(eID);
+
+        generateMessage(fName, lName, zipCode, eID);
+    }
+
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        getInput();
     }
 }
