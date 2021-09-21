9ff708c91bebb5983085ac3c161ce1cce287eb83
Ryan <rdturner40@gmail.com>
2021-09-21 10:04:41 -0400

    MVP Ex 27

--- src/main/java/ex28/App.java
+++ src/main/java/ex28/App.java
@@ -5,10 +5,44 @@ package ex28;
  *  Copyright 2021 Ryan Turner
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App
 {
-    public static void main( String[] args )
+    public static void createOutput(int sum) {
+        System.out.println("The total is " + sum);
+    }
+
+    public static void handleCalcs(int[] numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        createOutput(sum);
+    }
+
+    public static int[] getInput()throws IOException {
+        int[] numbers;
+        int numOfNumbers;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("How many numbers would you like to add?");
+        numOfNumbers = Integer.parseInt(reader.readLine());
+
+        numbers = new int[numOfNumbers];
+        
+        for (int i = 0; i < numOfNumbers; i++) {
+            int currentNum = i + 1;
+            System.out.println("Enter number " + currentNum);
+            numbers[i] = Integer.parseInt(reader.readLine());
+        }
+
+        return numbers;
+    }
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        int[] numbers = getInput();
+        handleCalcs(numbers);
     }
 }
