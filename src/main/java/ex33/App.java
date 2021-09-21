dc53b1b27a17ef174ad907d081b0cb7db539c865
Ryan <rdturner40@gmail.com>
2021-09-21 12:16:38 -0400

    MVP Ex 33

--- src/main/java/ex33/App.java
+++ src/main/java/ex33/App.java
@@ -5,10 +5,30 @@ package ex33;
  *  Copyright 2021 Ryan Turner
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 public class App
 {
-    public static void main( String[] args )
+    public static void getAnswer() {
+        String[] answers = {"Yes", "No", "Maybe", "Ask again later"};
+        Random random = new Random();
+        System.out.println(answers[random.nextInt(4)]);
+
+    }
+
+    public static void getUserInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String question;
+        System.out.println("What is your question? ");
+        question = reader.readLine();
+        getAnswer();
+    }
+
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        getUserInput();
     }
 }
