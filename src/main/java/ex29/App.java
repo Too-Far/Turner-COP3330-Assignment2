169417ff64fe86808d94778965edf520c5e6728b
Ryan <rdturner40@gmail.com>
2021-09-21 10:18:02 -0400

    MVP Ex 29

--- src/main/java/ex29/App.java
+++ src/main/java/ex29/App.java
@@ -5,10 +5,43 @@ package ex29;
  *  Copyright 2021 Ryan Turner
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App
 {
-    public static void main( String[] args )
+    public static void generateOutput(int years) {
+        System.out.println("It will take " + years + " years to double your initial investment.");
+    }
+
+    public static void calcReturn(int rate) {
+        int years = 72 / rate;
+        generateOutput(years);
+    }
+
+    public static void getInput() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int rate;
+        boolean flag = true;
+        while (flag) {
+            try {
+                System.out.println("What is the rate of return? ");
+                rate = Integer.parseInt(reader.readLine());
+                if (rate > 0) {
+                    calcReturn(rate);
+                    flag = false;
+                } else {
+                    System.out.println("Sorry that's not a valid input.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Sorry that's not a valid input");
+            }
+        }
+    }
+
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        getInput();
     }
 }
