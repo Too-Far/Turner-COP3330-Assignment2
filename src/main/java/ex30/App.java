d50d511f1d8906f8c705ecf32edbcb912f958b73
Ryan <rdturner40@gmail.com>
2021-09-21 10:34:54 -0400

    MVP Ex 30

--- src/main/java/ex30/App.java
+++ src/main/java/ex30/App.java
@@ -7,8 +7,26 @@ package ex30;
 
 public class App
 {
+    public static void printMultiplicationTable() {
+        System.out.format("      ");
+        for (int i = 1; i < 13; i++) {
+            System.out.format("%4d", i);
+        }
+        System.out.println();
+        System.out.println("      ------------------------------------------------");
+
+        for (int i = 1; i < 13; i++) {
+            System.out.format("%4d |", i);
+            for (int j = 1; j < 13; j++) {
+                int value = i * j;
+                System.out.format("%4d", value);
+            }
+            System.out.println();
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        printMultiplicationTable();
     }
 }
