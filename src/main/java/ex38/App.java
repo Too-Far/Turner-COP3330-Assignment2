feb4db40145c24a092912644e560fe50f075079f
Ryan <rdturner40@gmail.com>
2021-09-23 17:40:23 -0400

    MVP Ex 38

--- src/main/java/ex38/App.java
+++ src/main/java/ex38/App.java
@@ -5,10 +5,40 @@ package ex38;
  *  Copyright 2021 Ryan Turner
  */
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+class UserHandler
+{
+    public static int[] gatherInput() throws IOException {
+        String temp;
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter a list of numbers, separated by spaces: ");
+        temp = reader.readLine();
+        return Stream.of(temp.split(" ")).mapToInt(Integer::parseInt).toArray();
+    }
+}
+
 public class App
 {
-    public static void main( String[] args )
+    public static List<Integer> filterEvenNumbers(int[] nums) {
+        List<Integer> numbers = new ArrayList<>();
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                numbers.add(num);
+            }
+        }
+        return numbers;
+    }
+
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+        int[] numbers = UserHandler.gatherInput();
+        System.out.println("The even numbers are: " + Arrays.toString(filterEvenNumbers(numbers).toArray()).replace("[", "").replace("]", ""));
     }
 }
